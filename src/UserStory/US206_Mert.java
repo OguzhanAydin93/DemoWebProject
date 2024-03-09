d91b15df73205a67959def8a4f0c9817c4170c3d
MertOzmen <avmertozmen93@gmail.com>
2024-03-09 23:15:40 +0300

    14.1-inch laptop seçildi ve sepete eklendi ardından Shopping cart'a tıklandı.

--- src/UserStory/US206_Mert.java
+++ src/UserStory/US206_Mert.java
@@ -17,24 +17,39 @@ public class US206_Mert extends BaseDriver {
         Actions actionDriver=new Actions(driver);
 
         WebElement loginButton= driver.findElement(By.cssSelector("[class='ico-login']"));
-        actionDriver.moveToElement(loginButton).click().build().perform();
+        actionDriver.moveToElement(loginButton).click().perform();
 
-        WebElement inputEmail= driver.findElement(By.id("Email"));
-        actionDriver.moveToElement(inputEmail).sendKeys("mert123@gmail.com").build().perform();
+        WebElement inputEmail= driver.findElement(By.xpath("//input[@id='Email']"));
+        actionDriver.moveToElement(inputEmail).sendKeys("mert123@gmail.com").perform();
 
         WebElement inputPassword= driver.findElement(By.id("Password"));
-        actionDriver.moveToElement(inputPassword).sendKeys("pass123").build().perform();
+        actionDriver.moveToElement(inputPassword).click().sendKeys("pass123").perform();
 
         WebElement lgnBtn= driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
-        actionDriver.moveToElement(lgnBtn).click().build().perform();
+        actionDriver.moveToElement(lgnBtn).click().perform();
         bekle.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
 
-        WebElement computers= driver.findElement(By.linkText("Computers"));
+        WebElement computers= driver.findElement(By.linkText("COMPUTERS"));
         actionDriver.moveToElement(computers).build().perform();
 
         WebElement noteBooks= driver.findElement(By.linkText("Notebooks"));
         bekle.until(ExpectedConditions.elementToBeClickable(noteBooks));
         actionDriver.moveToElement(noteBooks).click().build().perform();
+
+        bekle.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/notebooks"));
+
+        WebElement laptop=driver.findElement(By.linkText("14.1-inch Laptop"));
+        actionDriver.moveToElement(laptop).click().perform();
+        bekle.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/141-inch-laptop"));
+
+        WebElement addToCart= driver.findElement(By.xpath("//input[@id='add-to-cart-button-31']"));
+        actionDriver.moveToElement(addToCart).click().build().perform();
+
+        WebElement shoppingCart= driver.findElement(By.xpath("//span[text()='Shopping cart']"));
+        actionDriver.moveToElement(shoppingCart).click().build().perform();
+        bekle.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/cart"));
+
+
 
 
 
