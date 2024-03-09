01f8a8253fb290c34c117ff0963e33bbe57cc655
MertOzmen <avmertozmen93@gmail.com>
2024-03-09 22:39:09 +0300

    Action oluşturuldu,Login butonu Locator'ı alındı ve giriş yapıldı.

--- src/UserStory/US206_Mert.java
+++ src/UserStory/US206_Mert.java
@@ -1,12 +1,25 @@
 package UserStory;
 
+import Utlity.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
-public class US206_Mert {
+public class US206_Mert extends BaseDriver {
 
     @Test
 
     public void SiparisVerme(){
+        driver.get("https://demowebshop.tricentis.com/");
+
+        Actions actionDriver=new Actions(driver);
+
+        WebElement loginButton= driver.findElement(By.cssSelector("[class='ico-login']"));
+        actionDriver.moveToElement(loginButton).click().build().perform();
+
+
+
 
     }
 
