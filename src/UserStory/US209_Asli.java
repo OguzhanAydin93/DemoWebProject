d627cd68aa561534bdfccbd9c469b6da2f42a409
asli003 <z.aslicelik@gmail.com>
2024-03-10 17:30:31 +0300

    Login islemi

--- src/UserStory/US209_Asli.java
+++ src/UserStory/US209_Asli.java
@@ -1,4 +1,41 @@
 package UserStory;
 
-public class US209_Asli {
+import Utlity.BaseDriver;
+import Utlity.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class US209_Asli extends BaseDriver {
+
+@Test
+    public void SiparisGecmisiLocalKontrol(){
+
+
+        driver.get("https://demowebshop.tricentis.com/");
+    Tools.Bekle(2);
+
+
+    Actions aksiyonDriver=new Actions(driver);
+
+
+    WebElement logIn=driver.findElement(By.cssSelector("[class='ico-login']"));
+    Action aksiyonlar=aksiyonDriver.moveToElement(logIn).click().build();
+    aksiyonlar.perform();
+
+    WebElement email=driver.findElement(By.cssSelector("[name='Email']"));
+    Action aksiyonlar2=aksiyonDriver.moveToElement(email).sendKeys("asli0@gmail.com").build();
+    aksiyonlar2.perform();
+
+
+
+
+
+
+
+
+
+}
 }
--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -27,27 +27,25 @@ public class BaseDriver {
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         Tools.Bekle(2);
-        LoginTest();
+       // LoginTest();
     }
 
-    public void LoginTest(){
-        System.out.println("Login Test başladı");
-        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
-        Tools.Bekle(2);
+   //public void LoginTest(){
+   //    System.out.println("Login Test başladı");
+   //    driver.get("https://demowebshop.tricentis.com/");
+   //    Tools.Bekle(2);
 
-        WebElement email=driver.findElement(By.id("input-email"));
-        email.sendKeys("testng1@gmail.com");
+   //    WebElement email=driver.findElement(By.id("Email"));
+   //    email.sendKeys("asli0@gmail.com");
 
-        WebElement password=driver.findElement(By.id("input-password"));
-        password.sendKeys("123qweasd");
+   //    WebElement password=driver.findElement(By.id("Password"));
+   //    password.sendKeys("asli2003");
 
-        WebElement loginBtn=driver.findElement(By.xpath("//input[@type='submit']")); //By.cssSelector("[value='Login']")
-        loginBtn.click();
+   //    WebElement loginBtn=driver.findElement(By.cssSelector("[value='Log in']")); //By.cssSelector("[value='Login']")
+   //    loginBtn.click();
 
-        wait.until(ExpectedConditions.titleIs("My Account"));
-        Assert.assertTrue(driver.getTitle().equals("My Account"));
-        System.out.println("Login Test bitti");
-    }
+
+ //   }
 
     @AfterClass
     public void KapanisIslemleri(){  // TearDown
