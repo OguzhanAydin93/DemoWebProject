4df11cc68345809a9f1279b3202c7c109fce1fcc
ZGK <zehragkrl@gmail.com>
2024-03-10 14:44:46 +0300

    Login button locator bulundu. Wait eklendi. Excellent locator bulundu.

--- src/UserStory/US_207_Zehra.java
+++ src/UserStory/US_207_Zehra.java
@@ -4,6 +4,7 @@ import Utlity.BaseDriver;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class US_207_Zehra extends BaseDriver {
     @Test
@@ -18,6 +19,15 @@ public class US_207_Zehra extends BaseDriver {
 
         WebElement password= driver.findElement(By.xpath("//input[@type='password']"));
         password.sendKeys("Q1q2q3q4.");
+
+        WebElement loginbtn= driver.findElement(By.xpath("//input[@value='Log in']"));
+        loginbtn.click();
+
+        bekle.until(ExpectedConditions.urlToBe("https://demowebshop.tricentis.com/"));
+        WebElement excellent=driver.findElement(By.xpath("//*[text()='Excellent']"));
+        excellent.click();
+
+        BekleVeKapat();
 
 
 
