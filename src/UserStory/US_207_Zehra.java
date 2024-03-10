6095018c0e81f28f266a9dec6224498d0f9765d1
ZGK <zehragkrl@gmail.com>
2024-03-10 14:11:02 +0300

    Login, email, password locatorları bulundu.

--- src/UserStory/US_207_Zehra.java
+++ src/UserStory/US_207_Zehra.java
@@ -1,7 +1,27 @@
 package UserStory;
 
+import Utlity.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class US_207_Zehra {
+public class US_207_Zehra extends BaseDriver {
+    @Test
+    public void SurveyResponse(){
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement login= driver.findElement(By.linkText("Log in"));
+        login.click();
+
+        WebElement email= driver.findElement(By.cssSelector("[id='Email']"));
+        email.sendKeys("zgkoroglu@gmail.com");
+
+        WebElement password= driver.findElement(By.xpath("//input[@type='password']"));
+        password.sendKeys("Q1q2q3q4.");
+
+
+
+
+    }
 
 }
