e31781d903da89288f9cd8400bd1fda12d1179b9
nurii <ernuri518@gmail.com>
2024-03-10 17:25:20 +0300

    Email şifre ile siteye girildi.

--- src/UserStory/US203_Nuri.java
+++ src/UserStory/US203_Nuri.java
@@ -1,6 +1,9 @@
 package UserStory;
 
 import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class US203_Nuri extends BaseDriver {
@@ -8,6 +11,26 @@ public class US203_Nuri extends BaseDriver {
     public void Test1()
     {
         driver.get("https://demowebshop.tricentis.com/");
+
+        Actions action=new Actions(driver);
+
+        WebElement login=driver.findElement(By.cssSelector("[class='ico-login']"));
+        action.moveToElement(login).click().perform();
+
+        WebElement eMail=driver.findElement(By.xpath("//input[@id='Email']"));
+        action.moveToElement(eMail).sendKeys("nuriabc@gmail.com").perform();
+
+        WebElement password=driver.findElement(By.id("Password"));
+        action.moveToElement(password).click().sendKeys("123456").perform();
+
+        WebElement loginbutton=driver.findElement(By.cssSelector("[class='button-1 login-button']"));
+        action.moveToElement(loginbutton).click().perform();
+
+
+
+
+
+
     }
 
 }
