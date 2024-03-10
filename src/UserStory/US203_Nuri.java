891b27782d45640bd295704cf784b9db30ef9ddf
nurii <ernuri518@gmail.com>
2024-03-10 13:43:35 +0300

    Web sayfasına giriş linki eklendi.

--- src/UserStory/US203_Nuri.java
+++ src/UserStory/US203_Nuri.java
@@ -1,4 +1,12 @@
 package UserStory;
 
-public class US203_Nuri {
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US203_Nuri extends BaseDriver {
+    @Test
+    public void Test1()
+    {
+        driver.get("https://demowebshop.tricentis.com/");
+    }
 }
