31d53b656c5aca4fb1d56e7da995e60a0cfa5067
MertOzmen <avmertozmen93@gmail.com>
2024-03-10 23:00:00 +0300

    Düzenlemeler yapıldı.

--- src/UserStory/US206_Mert.java
+++ src/UserStory/US206_Mert.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 
 
-import java.time.Duration;
+
 import java.util.ArrayList;
 import java.util.List;
 
