fa4be99e1c5d01784ce26d016c8d0cab50d2a20b
ZGK <zehragkrl@gmail.com>
2024-03-11 14:26:58 +0300

    Son düzenleme yapıldı proje tamamlandı.

--- src/UserStory/US_207_Zehra.java
+++ src/UserStory/US_207_Zehra.java
@@ -18,36 +18,32 @@ public class US_207_Zehra extends BaseDriver {
 
         Actions actionDriver = new Actions(driver);
 
-        WebElement login=driver.findElement(By.cssSelector("[class='ico-login']"));
+        WebElement login = driver.findElement(By.cssSelector("[class='ico-login']"));
         actionDriver.moveToElement(login).click().perform();
 
-        WebElement eMail=driver.findElement(By.xpath("//input[@id='Email']"));
-        actionDriver.moveToElement(eMail).sendKeys("medicanovo@gmail.com").perform();
+        WebElement eMail = driver.findElement(By.xpath("//input[@id='Email']"));
+        actionDriver.moveToElement(eMail).sendKeys("hello-world@gmail.com").perform();
 
-        WebElement password=driver.findElement(By.id("Password"));
+        WebElement password = driver.findElement(By.id("Password"));
         actionDriver.moveToElement(password).click().sendKeys("P12345").perform();
 
         WebElement loginbtn = driver.findElement(By.xpath("//input[@value='Log in']"));
         actionDriver.moveToElement(loginbtn).click().perform();
 
-       bekle.until(ExpectedConditions.urlContains("https://demowebshop.tricentis.com/"));
-       WebElement surveyExcellent = driver.findElement(By.xpath("//li[@class='answer']/input[@value='1']"));
-       WebElement voteButton = driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
-       WebElement pollSection = driver.findElement(By.xpath("//*[text()='Community poll']"));
-       if (surveyExcellent.isDisplayed())
-           actionDriver.moveToElement(surveyExcellent).click().moveToElement(voteButton).click().perform();
-       else
-           bekle.until(ExpectedConditions.visibilityOf(pollSection));
+        bekle.until(ExpectedConditions.urlContains("https://demowebshop.tricentis.com/"));
+        WebElement surveyExcellent = driver.findElement(By.xpath("//li[@class='answer']/input[@value='1']"));
+        actionDriver.moveToElement(surveyExcellent).click().perform();
 
-        // WebElement voteButton=driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
-        //actionDriver.moveToElement(voteButton).click().perform();
-        // WebElement pollSection= driver.findElement(By.xpath("//*[text()='Community poll']"));
-        // bekle.until(ExpectedConditions.visibilityOf(pollSection));
 
-       List<WebElement> answerElements = driver.findElements(By.xpath("//ul[@class='poll-results']/li[contains(text(),'vote(s)')]"));
-       for (WebElement pollAnswers : answerElements) {
-           System.out.println("Sonuçlar, " + pollAnswers.getText());
-       }
+        WebElement voteButton = driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
+        actionDriver.moveToElement(voteButton).click().perform();
+        WebElement pollSection = driver.findElement(By.xpath("//*[text()='Community poll']"));
+        bekle.until(ExpectedConditions.visibilityOf(pollSection));
+
+        List<WebElement> answerElements = driver.findElements(By.xpath("//ul[@class='poll-results']/li[contains(text(),'vote(s)')]"));
+        for (WebElement pollAnswers : answerElements) {
+            System.out.println("Sonuçlar, " + pollAnswers.getText());
+        }
 
         BekleVeKapat();
 
