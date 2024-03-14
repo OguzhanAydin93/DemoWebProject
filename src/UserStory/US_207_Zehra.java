e2e3bee332c1fa59621572b697b0455437e894aa
ZGK <zehragkrl@gmail.com>
2024-03-14 11:16:03 +0300

    yeni mail ile düzenlendi

--- src/UserStory/US_207_Zehra.java
+++ src/UserStory/US_207_Zehra.java
@@ -22,13 +22,17 @@ public class US_207_Zehra extends BaseDriver {
         actionDriver.moveToElement(login).click().perform();
 
         WebElement eMail = driver.findElement(By.xpath("//input[@id='Email']"));
-        actionDriver.moveToElement(eMail).sendKeys("hello-world@gmail.com").perform();
+        actionDriver.moveToElement(eMail).sendKeys("mehmetahmet@gmail.com").perform();
 
         WebElement password = driver.findElement(By.id("Password"));
         actionDriver.moveToElement(password).click().sendKeys("P12345").perform();
 
         WebElement loginbtn = driver.findElement(By.xpath("//input[@value='Log in']"));
         actionDriver.moveToElement(loginbtn).click().perform();
+
+        bekle.until(ExpectedConditions.urlContains("https://demowebshop.tricentis.com/"));
+        WebElement surveyExcellent = driver.findElement(By.xpath("//li[@class='answer']/input[@value='1']"));
+        actionDriver.moveToElement(surveyExcellent).click().perform();
 
         bekle.until(ExpectedConditions.urlContains("https://demowebshop.tricentis.com/"));
         WebElement surveyExcellent = driver.findElement(By.xpath("//li[@class='answer']/input[@value='1']"));
